2e938d4207acf20044e7470ddf602eedfa90dfa2
Udhayashankar <udhayashankarsk@gmail.com>
2025-06-26 10:05:11 +0000

    Add files via upload

---
+++ JerkinCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.jerkin_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerkinCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
